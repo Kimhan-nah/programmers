fdfa447ddd38175e94b35f64700c4fe62af26336
Kimhan-nah <skqkdldhf98@naver.com>
2021-03-31 16:18:54 +0900

    3/31

---
+++ lv_01/matrix_add.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2)
+{
+    vector<vector<int>> answer;
+
+    for (int row = 0; row < arr1.size(); row++)
+    {
+        answer.push_back({});
+        for (int col = 0; col < arr1[row].size(); col++)
+        {
+            answer[row].push_back(arr1[row][col] + arr2[row][col]);
+        }
+    }
+
+    return answer;
+}
